d29d94ddfaa02b8edb94d65ec3586081b33a7d00
mohammedkarim69! <mohammed.karim.bouzidi@gmail.com>
2023-10-19 16:26:50 +0200

    modificatio picture hover

---
+++ src/app/pictureUrl.ts
@@ -0,0 +1,32 @@
+type picture = { image: string; alt: string };
+
+export const pictureList: picture[] = [
+  {
+    image: '../assets/images/01.jpg',
+    alt: 'camion remorquant une voiture',
+  },
+
+  {
+    image: '../assets/images/03.jpg',
+    alt: 'camion remorquant une voiture',
+  },
+
+  {
+    image: '../assets/images/02.jpg',
+    alt: 'camion remorquant une voiture',
+  },
+
+  {
+    image: '../assets/images/05.jpg',
+    alt: 'camion remorquant une voiture',
+  },
+
+  {
+    image: '../assets/images/06.jpg',
+    alt: 'camion remorquant une voiture',
+  },
+  {
+    image: '../assets/images/06.jpg',
+    alt: 'camion remorquant une voiture',
+  },
+];
