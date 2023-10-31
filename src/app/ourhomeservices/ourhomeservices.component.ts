b0561e28dd19c3ed294643401b904c8926980137
mohammedkarim69! <mohammed.karim.bouzidi@gmail.com>
2023-10-31 16:16:08 +0100

    start carousel

--- src/app/ourhomeservices/ourhomeservices.component.ts
+++ src/app/ourhomeservices/ourhomeservices.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ourhomeservices',
   templateUrl: './ourhomeservices.component.html',
-  styleUrls: ['./ourhomeservices.component.scss']
+  styleUrls: ['./ourhomeservices.component.scss'],
 })
 export class OurhomeservicesComponent {
-
+  images = ['../../assets/images/panne-camion-transporteur-pendant-travail-utilisant-transporteur-ceinture-verrouillee-autre-voiture-verte.jpg', 
+  '../../assets/images/03.jpg', 
+  '../../assets/images/06.jpg']; 
+    currentIndex = 0; 
+      constructor() {   
+          setInterval(() => {    
+               this.currentIndex = (this.currentIndex + 1) % this.images.length;    
+               }, 4000); 
+              
+              
+              }
 }
