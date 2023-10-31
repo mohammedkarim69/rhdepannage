3161e98d1a1507ae46d7bf7d655c2baf168e88f5
mohammedkarim69! <mohammed.karim.bouzidi@gmail.com>
2023-10-31 11:09:31 +0100

    included arrayservices

---
+++ src/app/arrayservices.ts
@@ -0,0 +1,21 @@
+type service={title: string;img: string;description:string};
+
+export const myServices: service[] =  [
+  {
+    title: 'Dépannage et remorquage',
+    img: '../assets/images/panne-camion-transporteur-pendant-travail-utilisant-transporteur-ceinture-verrouillee-autre-voiture-verte.jpg',
+    description:
+      'Les véhicules hors d’usage sont des déchets qui doivent être débarrassés en raison de leur nuisance sur l’environnement et leurencombrement. Pour profiter d’un service d’enlèvement d’épaves gratuit dans le département du du 26,38 et du département 69, faites appel à RH DÉPANNAGE.',
+  },
+  {
+    title: "Enlèvement d'épave",
+    img: '../assets/images/auto-3734396_1280.jpg',
+    description:"Les véhicules hors d’usage sont des déchets qui doivent être débarrassés en raison de leur nuisance sur l’environnement et leur encombrement. Pour profiter d’un service d’enlèvement d’épaves gratuit dans le département du du 26,38 et du département 69, faites appel à RH DÉPANNAGE."
+  },
+  {
+    title:"rachat et revente de véhicule",
+    img:'../assets/images/pexels-avinash-patel-544542.jpg' ,
+    description:"En moyenne, les Français changent de voiture tous les 5 ans, ce qui implique généralement de se débarrasser de l’ancien. Pour tout besoin d’un professionnel en rachat et revente de véhicule dans le 38, confiez-vous à RH DÉPANNAGE."
+    
+  },
+];
