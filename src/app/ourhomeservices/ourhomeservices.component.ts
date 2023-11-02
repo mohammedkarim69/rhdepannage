6646f4872f62ad51d058408eb261cd209a40d9db
mohammedkarim69! <mohammed.karim.bouzidi@gmail.com>
2023-11-02 09:53:29 +0100

    carousel

--- src/app/ourhomeservices/ourhomeservices.component.ts
+++ src/app/ourhomeservices/ourhomeservices.component.ts
@@ -6,15 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./ourhomeservices.component.scss'],
 })
 export class OurhomeservicesComponent {
+
+  constructor() {   
+    setInterval(() => {    
+         this.currentIndex = (this.currentIndex + 1) % this.images.length;    
+         }, 5000); 
+        }
   images = ['../../assets/images/panne-camion-transporteur-pendant-travail-utilisant-transporteur-ceinture-verrouillee-autre-voiture-verte.jpg', 
   '../../assets/images/03.jpg', 
   '../../assets/images/06.jpg']; 
     currentIndex = 0; 
-      constructor() {   
-          setInterval(() => {    
-               this.currentIndex = (this.currentIndex + 1) % this.images.length;    
-               }, 4000); 
-              
-              
-              }
+      
 }
