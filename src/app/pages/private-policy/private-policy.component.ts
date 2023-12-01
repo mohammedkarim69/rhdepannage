a7d32fba1fa8ac4477e01433ff24917a34c0ff1e
mohammedkarim69! <mohammed.karim.bouzidi@gmail.com>
2023-12-01 09:39:12 +0100

    html css

--- src/app/pages/private-policy/private-policy.component.ts
+++ src/app/pages/private-policy/private-policy.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./private-policy.component.scss']
 })
 export class PrivatePolicyComponent {
-  title : string = "Confidentialité"
+  title : string = "Politique de confidentialité"
 }
