075807af2cff92640afef6a903850b751fbe8dc3
mohammedkarim69! <mohammed.karim.bouzidi@gmail.com>
2024-03-18 10:20:54 +0100

    modification css html

--- src/app/pictureUrl.ts
+++ src/app/pictureUrl.ts
@@ -2,31 +2,43 @@ type picture = { image: string; alt: string };
 
 export const pictureList: picture[] = [
   {
+    image: '../assets/images/photovoiturejaune.jpg',
+    alt: 'camion remorquant une voiture jaune',
+  },
+  {
+    image: '../assets/images/photodepmoto.jpg',
+    alt: 'camion remorquant une moto ',
+  },
+  {
     image: '../assets/images/01.jpg',
     alt: 'camion remorquant une voiture',
   },
 
+  // {
+  //   image: '../assets/images/03.jpg',
+  //   alt: 'camion remorquant une voiture',
+  // },
+
+  // {
+  //   image: '../assets/images/02.jpg',
+  //   alt: 'camion remorquant une voiture',
+  // },
+
   {
-    image: '../assets/images/03.jpg',
-    alt: 'camion remorquant une voiture',
+    image: '../assets/images/photodepcamion.jpg',
+    alt: 'camion remorquant une voiture jaune',
   },
 
   {
-    image: '../assets/images/02.jpg',
-    alt: 'camion remorquant une voiture',
-  },
-
-  {
-    image: '../assets/images/05.jpg',
-    alt: 'camion remorquant une voiture',
-  },
-
-  {
-    image: '../assets/images/06.jpg',
-    alt: 'camion remorquant une voiture',
+    image: '../assets/images/photodepdoublevoiture.jpg',
+    alt: 'camion remorquant deux voiture ',
   },
   {
-    image: '../assets/images/06.jpg',
-    alt: 'camion remorquant une voiture',
+    image: '../assets/images/photocamionboue.jpg',
+    alt: 'camion marron bloqué dans la boue ',
+  },
+  {
+    image: '../assets/images/photodepboue.jpg',
+    alt: 'camion remorquant deux voiture ',
   },
 ];
