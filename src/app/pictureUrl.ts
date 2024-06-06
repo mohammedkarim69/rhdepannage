52f6c21c70e426d7ea55a283efaf5e8e1731b1a2
mohammedkarim69! <mohammed.karim.bouzidi@gmail.com>
2024-06-06 13:38:48 +0200

    add picture fix email

--- src/app/pictureUrl.ts
+++ src/app/pictureUrl.ts
@@ -10,29 +10,15 @@ export const pictureList: picture[] = [
     alt: 'camion remorquant une moto ',
   },
   {
+    image: '../assets/images/porsche.jpg',
+    alt: 'camion remorquant une voiture',
+  },
+
+  {
     image: '../assets/images/01.jpg',
     alt: 'camion remorquant une voiture',
   },
 
-  // {
-  //   image: '../assets/images/03.jpg',
-  //   alt: 'camion remorquant une voiture',
-  // },
-
-  // {
-  //   image: '../assets/images/02.jpg',
-  //   alt: 'camion remorquant une voiture',
-  // },
-
-  {
-    image: '../assets/images/photodepcamion.jpg',
-    alt: 'camion remorquant une voiture jaune',
-  },
-
-  {
-    image: '../assets/images/photodepdoublevoiture.jpg',
-    alt: 'camion remorquant deux voiture ',
-  },
   {
     image: '../assets/images/photocamionboue.jpg',
     alt: 'camion marron bloqué dans la boue ',
