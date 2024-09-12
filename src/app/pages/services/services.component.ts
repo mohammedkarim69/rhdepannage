fd09bc00ef7ab833762a514218956d408886a24e
Guillaume Cometto <guillaume.cometto@sncf.fr>
2024-09-12 16:21:21 +0200

    correction demandees par client

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss'],
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit {
   title: string = 'Nos Services';
+
+  ngOnInit(): void {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
